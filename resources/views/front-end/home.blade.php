69407f3edf7a60937d1f025de18277902338140c
csemiraz <mirazul2013@gmail.com>
2022-11-05 13:41:52 +0600

    Home page featured news

--- resources/views/front-end/home.blade.php
+++ resources/views/front-end/home.blade.php
@@ -1,6 +1,8 @@
 @extends('front-end.layouts.app')
 @section('title', 'News Portal')
 @section('main_content')
+
+{{-- Home News Ticker --}}
 @if($setting_data->news_ticker_status=='Show')
 <div class="news-ticker-item">
     <div class="container">
@@ -31,78 +33,113 @@
 </div>
 @endif
 
+{{-- Featured News --}}
 <div class="home-main">
     <div class="container">
         <div class="row g-2">
             <div class="col-lg-8 col-md-12 left">
                 <div class="inner">
+                    @php
+                        $i=0;
+                    @endphp
+                    @foreach($post_data as $item)
+                    @php
+                        $i++;
+                    @endphp
+                    @if($i>1)
+                        @break;
+                    @endif
                     <div class="photo">
                         <div class="bg"></div>
-                        <img src="{{ asset('assets/front-end/') }}/uploads/n1.jpg" alt="">
+                        <img src="{{ asset('assets/images/'.$item->post_photo) }}" alt="">
                         <div class="text">
                             <div class="text-inner">
                                 <div class="category">
-                                    <span class="badge bg-success badge-sm">Politics</span>
+                                    <span class="badge bg-success badge-sm">{{ $item->rSubCategory->subcategory_name }}</span>
                                 </div>
-                                <h2><a href="">Top five ranked teams in world lined up to take part in competition</a></h2>
+                                <h2><a href="{{ route('news_detail', $item->id) }}">{{ $item->post_title }}</a></h2>
                                 <div class="date-user">
                                     <div class="user">
-                                        <a href="">Paul David</a>
+                                        @if($item->author_id==0)
+                                            @php
+                                            $user_data = \App\Models\Admin::where('id',$item->admin_id)->first();
+                                            @endphp
+                                        @else
+                                            @php
+                                            $user_data = \App\Models\Admni::where('id',$item->author_id)->first();
+                                            @endphp
+                                        @endif
+        
+                                        <a href="">{{ $user_data->name }}</a>
                                     </div>
                                     <div class="date">
-                                        <a href="">10 Jan, 2022</a>
+                                        @php
+                                            $st = strtotime($item->updated_at);
+                                            $date = date("j F, Y", $st);
+                                        @endphp
+                                        <a href="">{{ $date }}</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    @endforeach
+
                 </div>
             </div>
             <div class="col-lg-4 col-md-12">
+                @php
+                    $i=0;
+                @endphp
+                @foreach($post_data as $item)
+                @php
+                    $i++;
+                @endphp
+                
+                    @if($i==1)
+                        @continue;
+                    @endif
+            
+                    @if($i>3)
+                        @break;
+                    @endif
                 <div class="inner inner-right">
                     <div class="photo">
                         <div class="bg"></div>
-                        <img src="{{ asset('assets/front-end/') }}/uploads/n2.jpg" alt="">
+                        <img src="{{ asset('assets/images/'.$item->post_photo) }}" alt="">
                         <div class="text">
                             <div class="text-inner">
                                 <div class="category">
-                                    <span class="badge bg-success badge-sm">Politics</span>
+                                    <span class="badge bg-success badge-sm">{{ $item->rSubCategory->subcategory_name }}</span>
                                 </div>
-                                <h2><a href="">Top five ranked teams in world lined up to take part in competition</a></h2>
+                                <h2><a href="{{ route('news_detail', $item->id) }}">{{ $item->post_title }}</a></h2>
                                 <div class="date-user">
                                     <div class="user">
-                                        <a href="">Paul David</a>
+                                        @if($item->author_id==0)
+                                        @php
+                                        $user_data = \App\Models\Admin::where('id',$item->admin_id)->first();
+                                        @endphp
+                                    @else
+                                        @php
+                                        $user_data = \App\Models\Admni::where('id',$item->author_id)->first();
+                                        @endphp
+                                    @endif
+    
+                                    <a href="">{{ $user_data->name }}</a>
                                     </div>
                                     <div class="date">
-                                        <a href="">10 Jan, 2022</a>
+                                        @php
+                                            $st = strtotime($item->updated_at);
+                                            $date = date("j F, Y", $st);
+                                        @endphp
+                                        <a href="">{{ $date }}</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="inner inner-right">
-                    <div class="photo">
-                        <div class="bg"></div>
-                        <img src="{{ asset('assets/front-end/') }}/uploads/n3.jpg" alt="">
-                        <div class="text">
-                            <div class="text-inner">
-                                <div class="category">
-                                    <span class="badge bg-success badge-sm">Politics</span>
-                                </div>
-                                <h2><a href="">Top five ranked teams in world lined up to take part in competition</a></h2>
-                                <div class="date-user">
-                                    <div class="user">
-                                        <a href="">Paul David</a>
-                                    </div>
-                                    <div class="date">
-                                        <a href="">10 Jan, 2022</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
